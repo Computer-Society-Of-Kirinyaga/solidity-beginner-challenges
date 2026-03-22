4734d38fecba5ebf2094c47c0f3f628feb9744c0
Keren Irungu <162571907+Kenyai402@users.noreply.github.com>
2026-03-22 12:31:17 +0300

    Add Challenge 2: Functions and return values
    Focuses on writing simple Solidity functions.
    
    Users are required to implement a function that takes an input and returns a computed value.
    
    This reinforces understanding of function structure, parameters, and return types.

---
+++ challenge-2/FunctionChallenge.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FunctionChallenge {
+
+    uint public number;
+
+    // TODO:
+    // Create a function called doubleNumber
+    // It should take a number and return the value multiplied by 2
+
+}
