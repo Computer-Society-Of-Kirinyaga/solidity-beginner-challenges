c20a93d79aaa34b21c0dfb2a0666615ffc8abf1c
Keren Irungu <162571907+Kenyai402@users.noreply.github.com>
2026-03-22 12:35:35 +0300

    Add Challenge 3: Mapping and msg.sender
    Introduces mappings for storing data per user.
    
    The challenge requires implementing a deposit function that stores values using msg.sender as the key.
    
    Helps learners understand how smart contracts handle multiple users.

---
+++ challenge-3/MappingChallenge.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MappingChallenge {
+
+    mapping(address => uint) public balances;
+
+    // TODO:
+    // Create a function called deposit
+    // It should take an amount and store it in balances
+    // Use msg.sender
+
+}
