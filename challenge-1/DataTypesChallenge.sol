988ada363a86b92e7ac7d0821953adae8fc5f6de
Keren Irungu <162571907+Kenyai402@users.noreply.github.com>
2026-03-22 12:20:45 +0300

    Add challenge 1: data types and state variables

---
+++ challenge-1/DataTypesChallenge.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DataTypesChallenge {
+
+    uint public myNumber;
+    string public myText;
+    bool public isActive;
+
+    // TODO:
+    // Create a function called setValues
+    // It should take:
+    // - a number
+    // - a string
+    // - a boolean
+    // Then update all the variables above
+
+}
